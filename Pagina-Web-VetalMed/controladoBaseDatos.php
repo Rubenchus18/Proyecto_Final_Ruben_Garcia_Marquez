facd891ffa174811017413391ba70881ef4b5db0
Rubenchus18 <rubengarci327@gmail.com>
2025-03-20 23:23:33 +0100

    Añadido logueo de pagina

---
+++ Pagina-Web-VetalMed/controladoBaseDatos.php
@@ -0,0 +1,43 @@
+<?php
+
+$conexion = mysqli_connect("donoso005node.mooo.com:25570", "rubenchus18", "Unlx%bjXVnwt9H4bA", "ClinicaMedica");
+if (!$conexion) {
+    die("Conexión fallida: " . mysqli_connect_error());
+}
+
+
+$nombre = $_POST["nombre"];
+$contraseña = $_POST["contraseña"];
+$direccion = $_POST["direccion"];
+$telefono = $_POST["telefono"];
+$nacimiento = $_POST["nacimiento"]; 
+$rol = 'paciente';
+
+
+if (empty($nombre) || empty($contraseña) || empty($direccion) || empty($telefono) || empty($nacimiento)) {
+    die("Todos los campos son obligatorios.");
+}
+
+
+$contraseña_hasheada = password_hash($contraseña, PASSWORD_DEFAULT);
+
+$query_empleado = "INSERT INTO Empleados (username, password, rol) VALUES ('$nombre', '$contraseña_hasheada', '$rol')";
+if (mysqli_query($conexion, $query_empleado)) {
+    echo "Empleado insertado correctamente. ID: " . mysqli_insert_id($conexion) . "<br>";
+    $empleado_id = mysqli_insert_id($conexion);
+
+    $query_paciente = "INSERT INTO Pacientes (nombre, direccion, telefono, fecha_nacimiento, empleado_id) 
+                       VALUES ('$nombre', '$direccion', '$telefono', '$nacimiento', '$empleado_id')";
+    if (mysqli_query($conexion, $query_paciente)) {
+        echo "Paciente creado exitosamente.";
+    } else {
+        echo "Error al crear el paciente: " . mysqli_error($conexion) . "<br>";
+        echo "Consulta: " . $query_paciente;
+    }
+} else {
+    echo "Error al crear el empleado: " . mysqli_error($conexion) . "<br>";
+    echo "Consulta: " . $query_empleado;
+}
+
+mysqli_close($conexion);
+?>
