b8167c1b7a0f69470447a7c0512bd71660681c6a
Rubenchus18 <rubengarci327@gmail.com>
2025-04-21 23:11:32 +0200

    Mejora de pagina web

--- Pagina-Web-VetalMed/controladoBaseDatos.php
+++ Pagina-Web-VetalMed/controladoBaseDatos.php
@@ -5,7 +5,6 @@ if (!$conexion) {
     die("Conexión fallida: " . mysqli_connect_error());
 }
 
-
 $nombre = $_POST["nombre"];
 $contraseña = $_POST["contraseña"];
 $direccion = $_POST["direccion"];
@@ -13,23 +12,20 @@ $telefono = $_POST["telefono"];
 $nacimiento = $_POST["nacimiento"]; 
 $rol = 'paciente';
 
-
 if (empty($nombre) || empty($contraseña) || empty($direccion) || empty($telefono) || empty($nacimiento)) {
-    die("Todos los campos son obligatorios.");
+      header("Location: MalRegistrado.html");
+      exit();
 }
-
-
-
 
 $query_empleado = "INSERT INTO Empleados (username, password, rol) VALUES ('$nombre', '$contraseña', '$rol')";
 if (mysqli_query($conexion, $query_empleado)) {
-    echo "Empleado insertado correctamente. ID: " . mysqli_insert_id($conexion) . "<br>";
     $empleado_id = mysqli_insert_id($conexion);
 
     $query_paciente = "INSERT INTO Pacientes (nombre, direccion, telefono, fecha_nacimiento, empleado_id) 
                        VALUES ('$nombre', '$direccion', '$telefono', '$nacimiento', '$empleado_id')";
     if (mysqli_query($conexion, $query_paciente)) {
-        echo "Paciente creado exitosamente.";
+        header("Location: RegistradoCorrectamente.html");
+        exit();
     } else {
         echo "Error al crear el paciente: " . mysqli_error($conexion) . "<br>";
         echo "Consulta: " . $query_paciente;
